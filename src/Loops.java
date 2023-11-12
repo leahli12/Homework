e7f1672377d8637e57b40e8547d0032b06b6f2ea
leahli12 <leahruiya08@gmail.com>
2023-11-11 23:46:48 -0500

    Final loops hw

--- src/Loops.java
+++ src/Loops.java
@@ -4,6 +4,34 @@ public class Loops {
     }
 
     public Loops(){
-        
+        countUp();
+        System.out.println();
+        countByThrees();
+        System.out.println();
+        countDown();
+    }
+
+    public void countUp(){
+        for (int x = 1; x < 6; x += 1){
+            System.out.println(x);
+        }
+    }
+    public void countByThrees(){
+        for (int j = 3; j < 16; j += 3){
+            System.out.print(j + " ");
+        }
+    }
+
+    public void countDown(){
+        for (int h = 10; h > 0; h -= 1){
+            if (h == 1){
+                System.out.print(h);
+            }
+            else {
+                System.out.print(h + ", ");
+            }
+        }
+        System.out.println();
+        System.out.println("Happy Loop Year!");
     }
 }
